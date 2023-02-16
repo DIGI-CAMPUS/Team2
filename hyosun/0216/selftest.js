85e0f4c938cd9c248de0c5aca10707e74028abd1
abdevelo <talove0531@gmail.com>
2023-02-16 18:56:46 +0900

    heart

---
+++ hyosun/0216/selftest.js
@@ -0,0 +1,31 @@
+// function doubleNumber(myNumber) {
+//   let myResult = myNumber * 2 ;
+//   return myResult;
+// }
+
+// n = doubleNumber(3);
+// console.log(n);
+
+let n = 4;
+
+// function halNum (n){
+//   let myResult = n / 2 
+//   return myResult;
+// }
+
+function halfNum (n){
+  let myResult = n / 2 
+  return myResult;
+} 
+
+let a = halfNum(4);
+
+function halfAddNum (a){
+  let result = myResult + 1;
+  return result ; 
+}
+
+let b = halfAddNum (4);
+
+console.log(a);
+console.log(b);
